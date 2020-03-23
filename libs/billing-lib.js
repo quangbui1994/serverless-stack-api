676329b3498e022017a8ad33f6e5fe0436bcdcc6
Quang Bui <buiphuquang2412@gmail.com>
2020-03-23 12:41:32 +0200

    Adding our Serverless API

---
+++ libs/billing-lib.js
@@ -0,0 +1,11 @@
+export function calculateCost(storage) {
+    let rate;
+    if (storage <= 10) {
+        rate = 4;
+    } else if (storage <= 100) {
+        rate = 2;
+    } else {
+        rate = 1;
+    }
+    return rate * storage * 100;
+}
